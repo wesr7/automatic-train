5d8e4576dfdd8feff916e75e88041e8a11d5ad53
wesr7 <jwesleyrodgers@gmail.com>
2017-04-20 21:58:10 -0700

    another try

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,5 +1,5 @@
 const config = {
-  url : 'mongodb://root:root@ds111851.mlab.com:11851/heroku_j3zkdqnw'
+  url : 'mongodb://heroku_j3zkdqnw:11kpj20o24t42b2v1qf1gpmni2@ds111851.mlab.com:11851/heroku_j3zkdqnw'
 };
 
 export default config;
